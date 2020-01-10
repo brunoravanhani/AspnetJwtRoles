671e94c505464ba076f8294c21cfbeeebaa6ee9c
Bruno Ravanhani <bruno.ravanhanif@gmail.com>
2020-01-10 16:40:09 -0400

    initial

---
+++ AspnetJwtRoles/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿
+using AspnetJwtRoles.Models;
+using AspnetJwtRoles.Repositories;
+using AspnetJwtRoles.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspnetJwtRoles.Controllers
+{
+    [Route("v1/account")]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+            }
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new
+            {
+                user,
+                token
+            };
+        }
+    }
+}
---
+++ AspnetJwtRoles/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetJwtRoles.Controllers
+{
+    [Route("v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => string.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
